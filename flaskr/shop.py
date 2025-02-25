b5839524bbd8312228c86591c07d7beb2230d70f
GoodgamJ <goodgamj@bc.edu>
2025-02-25 14:09:39 -0500

    Added browsing by category and product search capabilities, as well as abillity to click on products to be linked to a description.

--- flaskr/shop.py
+++ flaskr/shop.py
@@ -8,7 +8,6 @@ from flaskr.db import get_db
 
 bp = Blueprint('shop', __name__)
 
-@bp.route('/')
 def index():
     db = get_db()
     products = db.execute(
@@ -17,3 +16,43 @@ def index():
         ' ORDER BY CategoryName DESC'
     ).fetchall()
     return render_template('shop/index.html', products=products)
+
+@bp.route('/')
+def browse_or_search():
+    db = get_db()
+    category_id = request.args.get('category')
+    search_query = request.args.get('search')
+
+    query = ('SELECT ProductID, ProductName, UnitPrice, c.CategoryID, CategoryName'
+        ' FROM Products p JOIN Categories c ON p.CategoryID = c.CategoryID')
+
+    params = []
+
+    if category_id:
+        query += ' WHERE p.CategoryID = ?'
+        params.append(category_id)
+    elif search_query:
+        query += ' WHERE p.ProductName LIKE ?'
+        params.append(f'%{search_query}%')
+
+    query += ' ORDER BY CategoryName'
+
+    products = db.execute(query, params).fetchall()
+
+    categories = db.execute('SELECT CategoryID, CategoryName FROM Categories').fetchall()
+
+    return render_template('shop/browse_or_search.html', products=products, categories=categories)
+
+@bp.route('/product/<int:product_id>')
+def product_details(product_id):
+    db = get_db()
+
+    product_info = db.execute(
+        '''SELECT ProductID, ProductName, UnitPrice, QuantityPerUnit
+           FROM Products
+           WHERE ProductID = ?''', (product_id,)).fetchone()
+
+    if product_info is None:
+        abort(404, "Item not available")
+
+    return render_template('shop/product_details.html', product_info=product_info)
