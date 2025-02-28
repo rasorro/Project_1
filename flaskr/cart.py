126a69eb4423a5478de97f43953d020818c48078
Charlie Keough <charliekeough@charlies-mbp-66.bc.edu>
2025-02-28 12:14:16 -0500

    Cart Fixes

--- flaskr/cart.py
+++ flaskr/cart.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
 from werkzeug.exceptions import abort
 
@@ -8,13 +8,22 @@ from flaskr.db import get_db
 
 bp = Blueprint('cart', __name__, url_prefix='/cart')
 
-@bp.route('/')
+@bp.route('/view', methods=('GET', 'POST'))
 def cart():
     db = get_db()
-    # cart_items = db.execute(
-    #    'SELECT * FROM Shoppint_Cart'
-    # ).fetchall()
-    return render_template('cart/cart.html')#, cart_items=cart_items)
+    cart_items = db.execute(
+        '''
+        SELECT sc.productID, p.ProductName, sc.quantity, p.UnitPrice, 
+                (sc.quantity * p.UnitPrice) AS TotalPrice
+        FROM Shopping_Cart sc
+        JOIN Products p ON sc.productID = p.ProductID
+        '''
+        ).fetchall()
+
+    # Debugging: Print the cart contents
+    print("Cart items:", cart_items)
+
+    return render_template('cart/cart.html', cart_items=cart_items)
 
 @bp.route('/checkout', methods=('GET', 'POST'))
 @login_required
@@ -30,3 +39,14 @@ def checkout():
         #db.execute(
         #    'INSERT INTO Orders (CustomerID, OrderDate, RequiredDate, ShippedDate, ShipVia, Freight, ShipName, ShipAddress, ShipCity, ShipRegion, ShipPostalCode, ShipCountry) VALUES (?,
     return render_template('cart/checkout.html')
+
+@bp.route('/remove/<int:product_id>', methods=['POST'])
+def remove_item(product_id):
+    db = get_db()
+    db.execute(
+        'DELETE FROM Shopping_Cart WHERE productID = ?',
+        (product_id,)
+    )
+    db.commit()
+    flash('Item removed from cart!')
+    return redirect(url_for('cart.cart'))
